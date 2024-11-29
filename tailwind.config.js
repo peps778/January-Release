a8bd9bd6f8badf09ca8e18b85406937ec2a68403
polks <magbanuatx56@gmail.com>
2024-11-29 15:57:48 -0500

    tailwind set up done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",         // Make sure to include your HTML file
+    "./js/**/*.js",         // If you're using JavaScript files, include them
+    "./css/**/*.css"        // Include your custom CSS if you plan to add custom styles
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
